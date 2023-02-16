028058f7f6937373645a55800e88a6631ed4c67a
Vincent <vdelupio@gmail.com>
2023-02-15 16:58:41 -0800

    Initial Commit

---
+++ 02_02/start/test/KeyboardAndMouseInput.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class KeyboardAndMouseInput {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/VDelu/repos/selenium/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+        WebElement name = driver.findElement(By.id("name"));
+        name.click();
+        name.sendKeys("Vince Delupio");
+
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+
+
+        driver.quit();
+    }
+}
---
+++ 06_01/start/test/Form.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Form {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/vdelu/repos/selenium/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        driver.findElement(By.id("first-name")).sendKeys("Vince");
+
+        driver.findElement(By.id("last-name")).sendKeys("Doe");
+
+        driver.findElement(By.id("job-title")).sendKeys("QA Manager");
+
+        driver.findElement(By.id("radio-button-2")).click();
+
+        driver.findElement(By.id("checkbox-1")).click();
+
+        driver.findElement(By.cssSelector("option[value='4']")).click();
+
+        driver.findElement(By.id("datepicker")).sendKeys("01/01/2024");
+
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        driver.quit();
+    }
+}
